c5c3d9f9dc4914500cd5594b5cc3562838438303
Bernard-VERA <bvmars@hotmail.com>
2024-03-27 00:26:18 +0100

    MongoDB + Mongoose et création user.js + book.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Début du paragraphe pour la connection a MongoDB
+const uri = "mongodb+srv://BernardV:13008@cluster0.ekvhmmx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+// Fin du paragraphe pour la connection a MongoDB
+
+// Donne un résultat au format JSON
 app.use(express.json());
 
+// MiddleWare CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
