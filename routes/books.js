18fd0cb7aabb1c1a7464a09d13b23ed9371f276e
Bernard-VERA <bvmars@hotmail.com>
2024-03-28 13:58:41 +0100

    Installation du middleware auth.js

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 const booksCtrl = require('../controllers/books');
 
-router.post('/', booksCtrl.createBook);
-router.get('/:id', booksCtrl.getOneBook);
-router.put('/:id', booksCtrl.modifyBook);
-router.delete('/:id', booksCtrl.deleteBook);
-router.get('/', booksCtrl.getAllBooks);
+router.post('/', auth, booksCtrl.createBook);
+router.get('/:id', auth, booksCtrl.getOneBook);
+router.put('/:id', auth, booksCtrl.modifyBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+router.get('/', auth, booksCtrl.getAllBooks);
 
 module.exports = router;
